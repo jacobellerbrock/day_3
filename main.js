b1d21fb64fbd6d56f03c9ae1218f054f99a4ad9d
Miguel Villasenor <97707721+miguel1453@users.noreply.github.com>
2023-05-31 12:04:18 +0100

    Added my own function

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//Jacobs function
 function printFibonnacci() {
     let x = 0, y = 1, z;
     for (let i = 0; i < 10; i++) {
@@ -9,3 +10,17 @@ function printFibonnacci() {
 }
 
 printFibonnacci();
+
+
+//Miguel Function
+const fibbonaci10 = () => {
+    let nums = [0, 1];
+
+    for (let i = 2; i < 10; i++) {
+        let num = nums[i-1] + nums[i-2];
+        nums.push(num);
+    }
+    return nums;
+};
+
+console.log(fibbonaci10());
